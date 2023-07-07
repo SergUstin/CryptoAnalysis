2f3fa28e12172500e3b012bc6957265229d4c45e
SergUstin <btboy_80@mail.ru>
2023-07-07 11:59:56 +0300

    code review

--- src/Bruteforce.java
+++ src/Bruteforce.java
@@ -11,42 +11,64 @@ import java.util.Scanner;
 
 public class Bruteforce {
 
-    private final Scanner scanner = new Scanner(System.in);
+//    private final Scanner scanner = new Scanner(System.in);
     private final CaesarCipher caesarCipher = new CaesarCipher();
     private static final int MAX_LENGTH_WORD = 28;
 
     public void bruteforce () throws IOException {
-        System.out.println("Введите путь к фалу для его расшифровки");
-        String path = scanner.nextLine();
+        StringBuilder builder = new StringBuilder();
+        List<String> strings = new ArrayList<>();
+        Util.print("Введите путь к фалу для его расшифровки");
+        String path = Util.readConsole();
 
-        Path bruteforce = PathHelper.buildFullName(path, "_bruteforce");
+        Path bruteforce = Util.buildFullName(path, "_bruteforce");
 
+        String str = Util.readFile(path);
+        builder.append(str).append(System.lineSeparator());
+        strings.add(str);
 
-        try (BufferedReader reader = Files.newBufferedReader(Paths.get(path));
-             BufferedWriter writer = Files.newBufferedWriter(bruteforce)) {
-            StringBuilder builder = new StringBuilder();
-            List<String> strings = new ArrayList<>();
-            while (reader.ready()) {
-                String string = reader.readLine();
-                builder.append(string).append(System.lineSeparator());
-                strings.add(string);
-            }
-            for (int i = 0; i < caesarCipher.alphabetLength(); i++) {
-                String decrypt = caesarCipher.decrypt(builder.toString(), i);
-                if (isValidateText(decrypt)) {
-                    for (String string : strings) {
-                        String encrypt = caesarCipher.decrypt(string, i);
-                        writer.write(encrypt + System.lineSeparator());
-                    }
-                    System.out.println("Содержимое файла расшифровано, методом подбора ключей. Ключ расшифровки K = "
-                            + i + System.lineSeparator());
-                    break;
+        for (int i = 0; i < caesarCipher.alphabetLength(); i++) {
+            String decrypt = caesarCipher.decrypt(builder.toString(), i);
+            if (isValidateText(decrypt)) {
+                for (String string : strings) {
+                    String encrypt = caesarCipher.decrypt(string, i);
+                    Util.writeFile( bruteforce,encrypt + System.lineSeparator());
                 }
+                Util.print("Содержимое файла расшифровано, методом подбора ключей. Ключ расшифровки K = "
+                        + i + System.lineSeparator());
+                break;
             }
         }
+
+
+
+
+//        try (BufferedReader reader = Files.newBufferedReader(Paths.get(path));
+//             BufferedWriter writer = Files.newBufferedWriter(bruteforce)) {
+//            StringBuilder builder = new StringBuilder();
+//            List<String> strings = new ArrayList<>();
+//            while (reader.ready()) {
+//                String string = reader.readLine();
+//                builder.append(string).append(System.lineSeparator());
+//                strings.add(string);
+//            }
+//            for (int i = 0; i < caesarCipher.alphabetLength(); i++) {
+//                String decrypt = caesarCipher.decrypt(builder.toString(), i);
+//                if (isValidateText(decrypt)) {
+//                    for (String string : strings) {
+//                        String encrypt = caesarCipher.decrypt(string, i);
+//                        writer.write(encrypt + System.lineSeparator());
+//                    }
+//                    Util.print("Содержимое файла расшифровано, методом подбора ключей. Ключ расшифровки K = "
+//                            + i + System.lineSeparator());
+//                    break;
+//                }
+//            }
+//        }
+
     }
 
-    private boolean isValidateText (String text) {
+    private boolean isValidateText (String text) throws IOException {
 
         boolean isValidate = false;
 
@@ -62,15 +84,15 @@ public class Bruteforce {
         while (isValidate) {
             int indexStart = new Random().nextInt(text.length() / 2);
             String substring = text.substring(indexStart, indexStart + (int) Math.sqrt(text.length()));
-            System.out.println(substring);
-            System.out.println("Понятен ли текст? (y/n)");
-            String answer = scanner.nextLine();
+            Util.print(substring);
+            Util.print("Понятен ли текст? (y/n)");
+            String answer = Util.readConsole();
             if (answer.equalsIgnoreCase("y")) {
                 return true;
             } else if (answer.equalsIgnoreCase("n")) {
                 isValidate = false;
             } else {
-                System.out.println("Выберите только между y или n");
+                Util.print("Выберите только между y или n");
             }
         }
         return false;
